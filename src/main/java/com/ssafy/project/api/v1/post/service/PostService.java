ddd50b3d1882f0ef1c8a32bd5fa02e6079cfc7b4
이영은 <youngeun.273@gmail.com>
2025-12-03 21:25:26 +0900

    Feat: post-create 작성

--- src/main/java/com/ssafy/project/api/v1/post/service/PostService.java
+++ src/main/java/com/ssafy/project/api/v1/post/service/PostService.java
@@ -1,20 +1,9 @@
 package com.ssafy.project.api.v1.post.service;
 
-import org.springframework.stereotype.Service;
-
 import com.ssafy.project.api.v1.post.dto.PostDto;
-import com.ssafy.project.api.v1.post.mapper.PostMapper;
 
-import lombok.RequiredArgsConstructor;
+public interface PostService {
 
-@Service
-@RequiredArgsConstructor
-public class PostService {
-	private final PostMapper postMapper;
-
-	public int create(PostDto dto) {
-		return postMapper.create(dto);
-		
-	}
+	Long createPost(PostDto dto, Long userId);
 
 }
