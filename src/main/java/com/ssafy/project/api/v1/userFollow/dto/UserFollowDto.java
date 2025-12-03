8c7c430527ee59297309b3953c541bdefb2e2888
Lani <nkw601@naver.com>
2025-12-03 16:14:47 +0900

    Revert "Rename: dto -> entity 변경"
    This reverts commit 980344f6db997016f5dd4680509b46a1a4df2d2c.

--- src/main/java/com/ssafy/project/api/v1/userFollow/dto/UserFollowDto.java
+++ src/main/java/com/ssafy/project/api/v1/userFollow/dto/UserFollowDto.java
@@ -1,4 +1,4 @@
-package com.ssafy.project.domain.userFollwer.entity;
+package com.ssafy.project.api.v1.userFollow.dto;
 
 import java.time.LocalDateTime;
 
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserFollowEntity {
+public class UserFollowDto {
     private Long followId;
     private Long followerId;
     private Long followeeId;
