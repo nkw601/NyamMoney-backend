c921a5a641c7a64045b8eafc62d79920edaabe74
이영은 <youngeun.273@gmail.com>
2025-12-03 21:25:26 +0900

    Feat: post-create 작성

--- backend/src/main/java/com/ssafy/project/api/v1/post/controller/PostController.java
+++ backend/src/main/java/com/ssafy/project/api/v1/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.ssafy.project.api.v1.post.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,18 @@ import com.ssafy.project.api.v1.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/post")
+@RequestMapping("/api/v1/boards/{boardId}/post")
 @RequiredArgsConstructor
 public class PostController {
 	private final PostService postService;
 	
-	public ResponseEntity<String> create(@RequestBody PostDto dto){
-		postService.create(dto);
-		return ResponseEntity.ok("created");
-	}
+	@PostMapping
+    public Long createPost(
+            @PathVariable Long boardId,
+            @RequestBody PostDto dto,
+            @AuthenticationPrincipal(expression = "userId") Long userId) {
+        dto.setBoardId(boardId);
+        return postService.createPost(dto, userId);
+    }
 
 }
--- backend/src/main/java/com/ssafy/project/api/v1/post/mapper/PostMapper.java
+++ backend/src/main/java/com/ssafy/project/api/v1/post/mapper/PostMapper.java
@@ -1,12 +1,13 @@
 package com.ssafy.project.api.v1.post.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.ssafy.project.api.v1.post.dto.PostDto;
 
 @Mapper
 public interface PostMapper {
 
-	int create(PostDto dto);
+	Long createPost(@Param("dto")PostDto dto, @Param("userId")Long userId);
 
 }
