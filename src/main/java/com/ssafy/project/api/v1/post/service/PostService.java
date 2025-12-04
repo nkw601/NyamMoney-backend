325f3ad6219ba19f08a34e56b6cc582eba080bbf
이영은 <youngeun.273@gmail.com>
2025-12-05 00:43:22 +0900

    Feat: post 조회 추가

--- src/main/java/com/ssafy/project/api/v1/post/service/PostService.java
+++ src/main/java/com/ssafy/project/api/v1/post/service/PostService.java
@@ -1,9 +1,12 @@
 package com.ssafy.project.api.v1.post.service;
 
+import com.ssafy.project.api.v1.post.dto.PostDetailResponse;
 import com.ssafy.project.api.v1.post.dto.PostDto;
 
 public interface PostService {
 
 	Long createPost(PostDto dto, Long userId);
+
+	PostDetailResponse getPostDetail(Long postId);
 
 }
