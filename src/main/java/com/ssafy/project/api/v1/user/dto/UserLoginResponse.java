f929e9b7c11a6cb0f8ed60e02b79c37f1239fb8a
Lani <nkw601@naver.com>
2025-12-04 16:18:33 +0900

    Feat: login req/res dto 추가

---
+++ src/main/java/com/ssafy/project/api/v1/user/dto/UserLoginResponse.java
@@ -0,0 +1,13 @@
+package com.ssafy.project.api.v1.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserLoginResponse {
+
+    private Long userId;
+    private String loginId;
+    private String nickname;
+}
