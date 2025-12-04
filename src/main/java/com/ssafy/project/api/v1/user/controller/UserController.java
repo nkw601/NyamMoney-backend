318439bb3b4a946ab83d282fad01f0696af985db
Lani <nkw601@naver.com>
2025-12-04 13:52:26 +0900

    Feat: UserController 생성

---
+++ src/main/java/com/ssafy/project/api/v1/user/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.ssafy.project.api.v1.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.project.api.v1.user.dto.UserDto;
+import com.ssafy.project.api.v1.user.dto.UserSignupRequest;
+import com.ssafy.project.api.v1.user.dto.UserSignupResponse;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+	
+	private final UserService uService;
+	
+	public UserController(UserService uService){
+		this.uService = uService;
+	}
+	
+	@PostMapping("/signup")
+	public ResponseEntity<UserSignupResponse> signup(@Valid @RequestBody UserSignupRequest req){
+		UserDto user = uService.signup(req);
+		
+		UserSignupResponse res = new UserSignupResponse(user.getUserId(), user.getNickname(), user.getCreatedAt());
+		
+		return ResponseEntity.ok(res);
+	}
+}
