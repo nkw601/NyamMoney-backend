3aa9bb6a603d7fe70b1366ad475e50a0d7219832
이영은 <youngeun.273@gmail.com>
2025-12-05 00:43:22 +0900

    Feat: post 조회 추가

--- backend/src/main/java/com/ssafy/project/api/v1/post/mapper/PostMapper.java
+++ backend/src/main/java/com/ssafy/project/api/v1/post/mapper/PostMapper.java
@@ -3,11 +3,14 @@ package com.ssafy.project.api.v1.post.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.ssafy.project.api.v1.post.dto.PostDetailResponse;
 import com.ssafy.project.api.v1.post.dto.PostDto;
 
 @Mapper
 public interface PostMapper {
 
 	Long createPost(@Param("dto")PostDto dto, @Param("userId")Long userId);
+
+	PostDetailResponse getPostDetail(Long postId);
 
 }
--- backend/src/main/java/com/ssafy/project/api/v1/post/service/PostServiceImpl.java
+++ backend/src/main/java/com/ssafy/project/api/v1/post/service/PostServiceImpl.java
@@ -2,21 +2,29 @@ package com.ssafy.project.api.v1.post.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.project.api.v1.post.dto.PostDetailResponse;
 import com.ssafy.project.api.v1.post.dto.PostDto;
 import com.ssafy.project.api.v1.post.mapper.PostMapper;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 	private final PostMapper postMapper;
+	public PostServiceImpl(PostMapper postMapper) {
+		this.postMapper = postMapper;
+	}
 
+	@Override
 	public Long createPost(PostDto dto, Long userId) {
 		dto.setUserId(userId);
 		postMapper.createPost(dto, userId);
-		return dto.getPostId(); // 생성된 PK 반
-		
+		return dto.getPostId(); // 생성된 PK 반환
+	}
+
+	@Override
+	public PostDetailResponse getPostDetail(Long postId) {
+		return postMapper.getPostDetail(postId);
 	}
 
 }
