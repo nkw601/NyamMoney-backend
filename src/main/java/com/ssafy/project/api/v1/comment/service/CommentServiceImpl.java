d1d4d8a3139a279fda3a4561f1c252d69c3f256c
이영은 <youngeun.273@gmail.com>
2025-12-08 16:39:38 +0900

    comment 등록

--- src/main/java/com/ssafy/project/api/v1/comment/service/CommentServiceImpl.java
+++ src/main/java/com/ssafy/project/api/v1/comment/service/CommentServiceImpl.java
@@ -2,6 +2,8 @@ package com.ssafy.project.api.v1.comment.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ssafy.project.api.v1.comment.dto.CommentCreateRequest;
+import com.ssafy.project.api.v1.comment.dto.CommentCreateResponse;
 import com.ssafy.project.api.v1.comment.dto.CommentDetailResponse;
 import com.ssafy.project.api.v1.comment.dto.CommentDto;
 import com.ssafy.project.api.v1.comment.mapper.CommentMapper;
@@ -18,6 +20,17 @@ public class CommentServiceImpl implements CommentService {
 	@Override
 	public CommentDetailResponse getComment(Long boardId, Long postId, Long commentId) {
 		return commentMapper.getComment(postId, commentId);
+	}
+
+	@Override
+	public CommentCreateResponse createComment(Long postId, Long userId, CommentCreateRequest req) {
+		CommentDto dto = new CommentDto();
+		dto.setPostId(postId);
+		dto.setUserId(userId);
+		dto.setContentMd(req.getContent());
+		
+		commentMapper.createComment(dto);
+		return new CommentCreateResponse(dto.getCommentId());
 	}
 
 }
