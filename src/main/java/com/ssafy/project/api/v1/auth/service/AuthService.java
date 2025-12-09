88e13247abfe3a861c422ecc2c71efaf626acc83
Gyungwon Na <nkw601@naver.com>
2025-12-09 15:36:27 +0900

    FeatL TokenRefresh Service 구현

--- src/main/java/com/ssafy/project/api/v1/auth/service/AuthService.java
+++ src/main/java/com/ssafy/project/api/v1/auth/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.ssafy.project.api.v1.auth.service;
 
+import com.ssafy.project.api.v1.auth.dto.TokenRefreshResponse;
+
 public interface AuthService {
 	public void logout(Long userId);
+	
+	public TokenRefreshResponse refresh(String refreshToken);
 }
