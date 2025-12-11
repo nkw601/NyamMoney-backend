15c3c5fbef63aa8bd66c04412949eb02ac93d368
이영은 <youngeun.273@gmail.com>
2025-12-11 16:01:58 +0900

    like 등록/삭제 토글 구현

--- src/main/java/com/ssafy/project/api/v1/postLike/service/PostLikeService.java
+++ src/main/java/com/ssafy/project/api/v1/postLike/service/PostLikeService.java
@@ -1,5 +1,9 @@
 package com.ssafy.project.api.v1.postLike.service;
 
+import com.ssafy.project.api.v1.postLike.dto.LikePostResponse;
+
 public interface PostLikeService {
+
+	LikePostResponse toggleLike(Long boardId, Long postId, Long userId);
 
 }
