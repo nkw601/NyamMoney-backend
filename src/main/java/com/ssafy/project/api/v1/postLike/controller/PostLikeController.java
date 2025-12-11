83053bad55f2978a069908ae57e83c21f597234d
이영은 <youngeun.273@gmail.com>
2025-12-11 23:36:48 +0900

    Feat: postLikeInfo 구현

--- src/main/java/com/ssafy/project/api/v1/postLike/controller/PostLikeController.java
+++ src/main/java/com/ssafy/project/api/v1/postLike/controller/PostLikeController.java
@@ -1,7 +1,9 @@
 package com.ssafy.project.api.v1.postLike.controller;
 
+import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.project.api.v1.postLike.dto.LikePostResponse;
 import com.ssafy.project.api.v1.postLike.service.PostLikeService;
 import com.ssafy.project.security.auth.UserPrincipal;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/api/v1/boards/{boardId}/posts/{postId}/like")
@@ -28,6 +28,16 @@ public class PostLikeController {
 														.getPrincipal();
 		Long userId = principal.getUserId();
 		LikePostResponse response = postLikeService.toggleLike(boardId, postId, userId);
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping
+	public ResponseEntity<LikePostResponse> getLikeInfo(@PathVariable Long boardId, @PathVariable Long postId) throws NotFoundException{
+		UserPrincipal principal = (UserPrincipal) SecurityContextHolder.getContext()
+														.getAuthentication()
+														.getPrincipal();
+		Long userId = principal.getUserId();
+		LikePostResponse response = postLikeService.getLikeInfo(boardId, postId, userId);
 		return ResponseEntity.ok(response);
 	}
 
