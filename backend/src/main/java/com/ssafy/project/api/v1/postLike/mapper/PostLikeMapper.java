6e6b95ff330a30039650a2a7dce74d85fe7e0056
이영은 <youngeun.273@gmail.com>
2025-12-11 16:01:58 +0900

    like 등록/삭제 토글 구현

--- backend/src/main/java/com/ssafy/project/api/v1/postLike/mapper/PostLikeMapper.java
+++ backend/src/main/java/com/ssafy/project/api/v1/postLike/mapper/PostLikeMapper.java
@@ -2,7 +2,14 @@ package com.ssafy.project.api.v1.postLike.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+
 @Mapper
-public class PostLikeMapper {
+public interface PostLikeMapper {
+
+	Long findLike(Long postId, Long userId);
+
+	void insertLike(Long postId, Long userId);
+
+	void deleteLike(Long likedId);
 
 }
