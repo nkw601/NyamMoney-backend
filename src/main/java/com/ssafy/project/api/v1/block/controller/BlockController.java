2682354cf8e765cd205728f2f7159c8ad1916aa5
나경원 <nkw601@naver.com>
2025-12-17 03:13:53 +0900

    Feat: block controller 구현

--- src/main/java/com/ssafy/project/api/v1/block/controller/BlockController.java
+++ src/main/java/com/ssafy/project/api/v1/block/controller/BlockController.java
@@ -1,9 +1,15 @@
 package com.ssafy.project.api.v1.block.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.project.api.v1.block.service.BlockService;
+import com.ssafy.project.api.v1.follow.dto.FollowOperationResponse;
+import com.ssafy.project.security.auth.UserPrincipal;
 
 @RestController
 @RequestMapping("api/v1/blocks")
@@ -13,5 +19,14 @@ public class BlockController {
 	
 	public BlockController(BlockService blockService) {
 		this.blockService = blockService;
+	}
+	
+	@PutMapping("/{targetUserId}")
+	public ResponseEntity<FollowOperationResponse> block(@PathVariable long targetUserId, @AuthenticationPrincipal UserPrincipal principal){
+		Long userId = principal.getUserId();
+		
+		FollowOperationResponse res = blockService.block(userId, targetUserId);
+		
+		return ResponseEntity.ok(res);
 	}
 }
