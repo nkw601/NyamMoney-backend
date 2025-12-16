d245e33e0c8ee71534f108a80be79f9ddaac596c
나경원 <nkw601@naver.com>
2025-12-17 03:35:11 +0900

    Feat: block service 구현

--- src/main/java/com/ssafy/project/api/v1/block/service/BlockServiceImpl.java
+++ src/main/java/com/ssafy/project/api/v1/block/service/BlockServiceImpl.java
@@ -1,5 +1,40 @@
 package com.ssafy.project.api.v1.block.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.ssafy.project.api.v1.follow.dto.FollowOperationResponse;
+import com.ssafy.project.api.v1.follow.mapper.FollowMapper;
+import com.ssafy.project.api.v1.user.dto.UserDto;
+import com.ssafy.project.api.v1.user.mapper.UserMapper;
+
 public class BlockServiceImpl implements BlockService {
+	
+	private final FollowMapper followMapper;
+	private final UserMapper userMapper;
+	
+	public BlockServiceImpl(FollowMapper followMapper, UserMapper userMapper) {
+		this.followMapper = followMapper;
+		this.userMapper = userMapper;
+	}
+	
+	@Override
+	@Transactional
+	public FollowOperationResponse block(Long userId, long targetUserId) {
+		if(userId.equals(targetUserId)) throw new ResponseStatusException(HttpStatus.FORBIDDEN, "자기 자신을 차단할 수 없습니다.");
+		
+		UserDto targetUser = userMapper.findById(targetUserId);
+		if(targetUser == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
+		
+		// block 처리
+		followMapper.putBlocked(userId, targetUserId);
+		
+		// target -> 나 팔로우(요청) 하고 있었다면 끊기
+		followMapper.deletePendingOrAccepted(targetUserId, userId);
+		
+		
+        return new FollowOperationResponse(targetUserId, "BLOCKED");
+	}
 
 }
