f2e57952a92aaf9add6ff76b506c809f889e048d
Gyungwon Na <nkw601@naver.com>
2025-12-16 16:44:10 +0900

    Feat: 팔로우 status enum 추가

--- src/main/java/com/ssafy/project/api/v1/follow/dto/FollowCreateResponse.java
+++ src/main/java/com/ssafy/project/api/v1/follow/dto/FollowCreateResponse.java
@@ -2,11 +2,24 @@ package com.ssafy.project.api.v1.follow.dto;
 
 import java.time.LocalDateTime;
 
+import com.ssafy.project.domain.follow.model.Status;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class FollowCreateResponse {
-	Long followId;
-	Long followerId;
-	Long followeeId;
-	String status; // 'pending', 'accepted', 'rejected', 'blocked'
+	private Long followId;
+	private Long followerId;
+	private Long followeeId;
+	private Status status; // 'pending', 'accepted', 'rejected', 'blocked'
     private LocalDateTime createdAt;
 
 }
---
+++ src/main/java/com/ssafy/project/domain/follow/model/Status.java
@@ -0,0 +1,5 @@
+package com.ssafy.project.domain.follow.model;
+
+public enum Status {
+	PENDING, ACCEPTED, REJECTED, BLOCKED
+}
