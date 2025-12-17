85b82ab11552d361d1ed697810afac927a7ac409
나경원 <nkw601@naver.com>
2025-12-17 21:01:36 +0900

    Feat: 유저 포스트 dto 구현

---
+++ backend/src/main/java/com/ssafy/project/common/dto/CursorPage.java
@@ -0,0 +1,16 @@
+package com.ssafy.project.common.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+@Setter
+public class CursorPage<T> {
+
+    private final List<T> items;      // 이번 응답 데이터
+    private final String nextCursor;  // 다음 요청에 사용할 커서 (없으면 null)
+    private final boolean hasNext;    // 다음 페이지 존재 여부
+}
