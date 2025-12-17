85eeda0dc365b51000a84f2a6e98e38d5131d33f
나경원 <nkw601@naver.com>
2025-12-18 02:48:08 +0900

    Feat: transcation post dto 추가

---
+++ src/main/java/com/ssafy/project/api/v1/transaction/dto/TransactionCreateResponse.java
@@ -0,0 +1,37 @@
+package com.ssafy.project.api.v1.transaction.dto;
+
+import java.time.LocalDateTime;
+
+import com.ssafy.project.api.v1.category.dto.CategoryItem;
+import com.ssafy.project.domain.transaction.model.TxType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class TransactionCreateResponse {
+	private Long transactionId;
+
+    private TxType txType;
+
+    private Long amount;
+
+    private LocalDateTime occurredAt;
+
+    private CategoryItem category;
+
+    private String merchantName;
+
+    private String memo;
+
+    private Boolean isRefund;
+
+    private LocalDateTime canceledAt;
+
+    private LocalDateTime createdAt;
+}
