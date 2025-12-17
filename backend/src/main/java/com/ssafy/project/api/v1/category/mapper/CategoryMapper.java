ca0a26e13dfef6fc3a2ae328f45de01cbc9d8d18
나경원 <nkw601@naver.com>
2025-12-18 02:27:58 +0900

    Feat: category 조회 mapper 구현

--- backend/src/main/java/com/ssafy/project/api/v1/category/mapper/CategoryMapper.java
+++ backend/src/main/java/com/ssafy/project/api/v1/category/mapper/CategoryMapper.java
@@ -1,8 +1,17 @@
 package com.ssafy.project.api.v1.category.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.ssafy.project.api.v1.category.dto.CategoryItem;
 
 @Mapper
 public interface CategoryMapper {
+
+	List<CategoryItem> selectAll();
+	
+    boolean existsById(@Param("categoryId") Long categoryId);
 
 }
