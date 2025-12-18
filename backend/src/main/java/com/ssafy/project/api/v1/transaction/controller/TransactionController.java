7ad70ba56ceeefd5b8539ecce7813831f2b6cde0
Gyungwon Na <nkw601@naver.com>
2025-12-19 02:30:48 +0900

    Chore: 패키지명 수정

--- backend/src/main/java/com/ssafy/project/api/v1/transaction/controller/TransactionController.java
+++ backend/src/main/java/com/ssafy/project/api/v1/transaction/controller/TransactionController.java
@@ -1,4 +1,4 @@
-package com.ssafy.project.api.v1.transaction.Controller;
+package com.ssafy.project.api.v1.transaction.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
