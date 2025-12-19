176defc0230982e97649e97fc763e4793b4ebe45
나경원 <nkw601@naver.com>
2025-12-19 17:10:08 +0900

    Feat: api -> category mapping 서비스 구현

---
+++ src/main/java/com/ssafy/project/api/v1/category/api/service/ApiDataService.java
@@ -0,0 +1,5 @@
+package com.ssafy.project.api.v1.category.api.service;
+
+public interface ApiDataService {
+
+}
---
+++ src/main/java/com/ssafy/project/api/v1/category/api/service/ApiDataServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ssafy.project.api.v1.category.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiDataServiceImpl implements ApiDataService {
+
+	
+}
