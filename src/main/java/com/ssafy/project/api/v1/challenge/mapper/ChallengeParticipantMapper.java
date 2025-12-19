a43a039a4fc643e735d6e948a203ee0452c1414b
youngeunee <youngeun.273@gmail.com>
2025-12-19 17:39:56 +0900

    Feat: 챌린지 단건 상세 정보 조회 구현

--- src/main/java/com/ssafy/project/api/v1/challenge/mapper/ChallengeParticipantMapper.java
+++ src/main/java/com/ssafy/project/api/v1/challenge/mapper/ChallengeParticipantMapper.java
@@ -3,9 +3,9 @@ package com.ssafy.project.api.v1.challenge.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.ssafy.project.api.v1.challenge.dto.participant.MyChallengeItem;
-import com.ssafy.project.domain.challenge.model.ChallengeStatus;
 import com.ssafy.project.domain.challengeParticipant.ChallengeParticipantStatus;
 
 @Mapper
@@ -13,15 +13,21 @@ public interface ChallengeParticipantMapper {
 
 	List<MyChallengeItem> selectMyChallenges(Long userId);
 
-	int existsParticipant(Long challengeId, Long userId);
+	int existsParticipant(@Param("userId") Long userId, @Param("challengeId") Long challengeId);
 
-	void insertParticipant(Long challengeId, Long userId);
+	void insertParticipant(@Param("challengeId") Long challengeId, @Param("userId") Long userId);
 
 	// 챌린지 삭제하는 경우 그 챌린지 참여한 모두 상태 업데이트 (userId 안 받음)
 	void updateParticipantStatusByDelete(Long challengeId, ChallengeParticipantStatus status);
+	
 	// 챌린지 참여 취소하는 경우 상태 업데이트 (userId 받음)
-	void updateParticipantStatus(Long challengeId, Long userId, ChallengeParticipantStatus status);
+	void updateParticipantStatus(@Param("challengeId") Long challengeId, @Param("userId") Long userId,
+			@Param("status") ChallengeParticipantStatus status);
 
-	ChallengeParticipantStatus selectParticipantStatus(Long challengeId, Long userId);
+	ChallengeParticipantStatus selectParticipantStatus(@Param("challengeId") Long challengeId, @Param("userId") Long userId);
+
+	int countParticipants(Long challengeId);
+
+	int JOINEDParticipant(Long userId, Long challengeId);
 
 }
--- src/main/java/com/ssafy/project/api/v1/challenge/service/ChallengeService.java
+++ src/main/java/com/ssafy/project/api/v1/challenge/service/ChallengeService.java
@@ -2,6 +2,7 @@ package com.ssafy.project.api.v1.challenge.service;
 
 import com.ssafy.project.api.v1.challenge.dto.challenge.ChallengeCreateRequest;
 import com.ssafy.project.api.v1.challenge.dto.challenge.ChallengeCreateResponse;
+import com.ssafy.project.api.v1.challenge.dto.challenge.ChallengeDetailResponse;
 import com.ssafy.project.api.v1.challenge.dto.challenge.ChallengeListResponse;
 import com.ssafy.project.api.v1.challenge.dto.challenge.ChallengeUpdateRequest;
 
@@ -16,5 +17,7 @@ public interface ChallengeService {
 	void updateChallenge(Long challengeId, @Valid ChallengeUpdateRequest request, Long userId) throws Exception;
 
 	void deleteChallenge(Long challengeId, Long userId) throws Exception;
+
+	ChallengeDetailResponse getChallengeDetail(Long challengeId, Long userId);
 
 }
