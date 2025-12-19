345dc0f5cb934cc9920af388a09895b0a46938b8
youngeunee <youngeun.273@gmail.com>
2025-12-19 11:04:15 +0900

    Feat: 참여한 챌린지 조회 구현

--- src/main/java/com/ssafy/project/api/v1/challenge/mapper/ChallengeParticipantMapper.java
+++ src/main/java/com/ssafy/project/api/v1/challenge/mapper/ChallengeParticipantMapper.java
@@ -1,8 +1,14 @@
 package com.ssafy.project.api.v1.challenge.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.project.api.v1.challenge.dto.participant.MyChallengeItem;
 
 @Mapper
 public interface ChallengeParticipantMapper {
+
+	List<MyChallengeItem> selectMyChallenges(Long userId);
 
 }
