9051e5b074ca111cb28477e184b06cb1b0517ef9
나경원 <nkw601@naver.com>
2025-12-22 10:36:04 +0900

    Feat: 농협 api 조회 dto 구현

---
+++ src/main/java/com/ssafy/project/api/v1/integration/nhcard/caller/NhCardCaller.java
@@ -0,0 +1,5 @@
+package com.ssafy.project.api.v1.integration.nhcard.caller;
+
+public class NhCardCaller {
+
+}
---
+++ src/main/java/com/ssafy/project/api/v1/integration/nhcard/service/NhCardService.java
@@ -0,0 +1,5 @@
+package com.ssafy.project.api.v1.integration.nhcard.service;
+
+public interface NhCardService {
+
+}
